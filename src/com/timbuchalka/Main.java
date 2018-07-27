60c9c7c4cb9f3c1fd6e40b6aa79da1dca5ef2429
bieradek <biedrzycki.radek@gmail.com>
2018-07-27 18:21:35 +0200

    Last changes

--- src/com/timbuchalka/Main.java
+++ src/com/timbuchalka/Main.java
@@ -4,5 +4,17 @@ public class Main {
 
     public static void main(String[] args) {
 
+        String input = "2018.125";
+        double number = Double.parseDouble(input);
+        //int number = Integer.parseInt(input);
+
+        System.out.println(input);
+        System.out.println(number);
+
+        input += 1;
+        number += 1;
+
+        System.out.println(input);
+        System.out.println(number);
     }
 }
